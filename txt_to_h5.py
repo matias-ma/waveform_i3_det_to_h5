cc6ba3827cefaf142d143d83875a81746a8e0886
Matias Andia <107262921+matma14@users.noreply.github.com>
2022-08-15 10:38:48 -0400

    Update txt_to_h5.py

--- txt_to_h5.py
+++ txt_to_h5.py
@@ -3,16 +3,39 @@
 import pandas as pd 
 import os
 
-os.mkdir('./csv/')
+if os.path.exists('/home/mandia/waveform/hnl/csv') is True: 
+    os.system('rm -r /home/mandia/waveform/hnl/csv')
+    os.mkdir('/home/mandia/waveform/hnl/csv')
+else:   
+    os.mkdir('/home/mandia/waveform/hnl/csv')
 
-df_charge = pd.read_csv('./txt/charge.txt')
-df_charge.to_csv('./csv/charge.csv', index = None)
+if os.path.exists('/home/mandia/waveform/hnl/h5') is True: 
+    os.system('rm -r /home/mandia/waveform/hnl/h5')
+    os.mkdir('/home/mandia/waveform/hnl/h5')
+else:   
+    os.mkdir('/home/mandia/waveform/hnl/h5')
+    
 
-df_time = pd.read_csv('./txt/time.txt')
-df_time.to_csv('./csv/time.csv', index = None)
+for i in range(1,10):
 
-df_position = pd.read_csv('./txt/position.txt')
-df_position.to_csv('./csv/position.csv', index = None)
+    for x in range(1,25):
+        os.mkdir('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x))
+        os.mkdir('/home/mandia/waveform/hnl/h5/h5_'+str(i)+'_'+str(x))
 
-df_time_nop = pd.read_csv('./txt/time_nop.txt')
-df_time_nop.to_csv('./csv/time_nop.csv', index = None)
+        df_charge = pd.read_csv('/home/mandia/waveform/hnl/txt/txt'+str(i)+'_'+str(x)+'/charge.txt')
+        c_charge = df_charge.to_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/charge.csv', index = None)
+        h_charge = pd.read_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/charge.csv').to_hdf('/home/mandia/waveform/hnl/h5/h5_'+str(i)+'_'+str(x)+'/charge.hdf5', key='data')
+
+        df_time = pd.read_csv('/home/mandia/waveform/hnl/txt/txt'+str(i)+'_'+str(x)+'/time.txt')
+        c_time = df_time.to_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/time.csv', index = None)
+        h_time = pd.read_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/time.csv').to_hdf('/home/mandia/waveform/hnl/h5/h5_'+str(i)+'_'+str(x)+'/time.hdf5', key='data')
+
+        df_position = pd.read_csv('/home/mandia/waveform/hnl/txt/txt'+str(i)+'_'+str(x)+'/position.txt')
+        c_position = df_position.to_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/position.csv', index = None)
+        h_position = pd.read_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/position.csv').to_hdf('/home/mandia/waveform/hnl/h5/h5_'+str(i)+'_'+str(x)+'/position.hdf5', key='data')
+
+        df_time_nop = pd.read_csv('/home/mandia/waveform/hnl/txt/txt'+str(i)+'_'+str(x)+'/time_nop.txt')
+        c_time_nop = df_time_nop.to_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/time_nop.csv', index = None)
+        h_time_nop = pd.read_csv('/home/mandia/waveform/hnl/csv/csv'+str(i)+'_'+str(x)+'/time_nop.csv').to_hdf('/home/mandia/waveform/hnl/h5/h5_'+str(i)+'_'+str(x)+'/time_nop.hdf5', key='data')
+        
+
